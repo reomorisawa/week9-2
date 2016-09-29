c5cd53eed8fe34067047e202671f4e38722bf1d2
reomorisawa <kuropon.africa@gmail.com>
2016-09-29 11:42:37 +0000

    add tweet resouces

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  resource :tweets
   resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
